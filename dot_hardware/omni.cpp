6c5b2c1f00b6a0f00838bfde6e746ab3b184a0bd
hex-plex <sendhilsom7@gmail.com>
2021-09-17 17:35:02 +0530

    added

--- dot_hardware/omni.cpp
+++ dot_hardware/omni.cpp
@@ -1,21 +1,51 @@
 #include "motor.h"
 #include <wiringPi.h>
 
-Motor* a;
+Motor* l,r,b;
 
-void updateEncoderA(){    
-    int MSB = digitalRead(a->motor_encA);
-    int LSB = digitalRead(a->motor_encB);
+void updateEncoderL(){    
+    int MSB = digitalRead(l->motor_encA);
+    int LSB = digitalRead(l->motor_encB);
 
     int encoded = (MSB<<1)|LSB;
-    int sum = (a->inter_val<<2)|encoded;
+    int sum = (l->inter_val<<2)|encoded;
     
     if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011)
-        a->pos--;
+        l->pos--;
     if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000)
-        a->pos++;
+        l->pos++;
 
-    a->inter_val = encoded;
+    l->inter_val = encoded;
+}
+
+void updateEncoderB(){    
+    int MSB = digitalRead(b->motor_encA);
+    int LSB = digitalRead(b->motor_encB);
+
+    int encoded = (MSB<<1)|LSB;
+    int sum = (b->inter_val<<2)|encoded;
+    
+    if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011)
+        b->pos--;
+    if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000)
+        b->pos++;
+
+    b->inter_val = encoded;
+}
+
+void updateEncoderR(){    
+    int MSB = digitalRead(r->motor_encA);
+    int LSB = digitalRead(r->motor_encB);
+
+    int encoded = (MSB<<1)|LSB;
+    int sum = (r->inter_val<<2)|encoded;
+    
+    if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011)
+        r->pos--;
+    if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000)
+        r->pos++;
+
+    r->inter_val = encoded;
 }
 
 int main(){
@@ -25,12 +55,20 @@ int main(){
     wiringPiSetup();
 #endif
 
-    a = new Motor(pi_22, pi_23, pi_27, pi_17, pi_4, 10.0, 100.0, 0.0);
-    wiringPiISR(a->motor_encA, INT_EDGE_BOTH, updateEncoderA);
-    wiringPiISR(a->motor_encB, INT_EDGE_BOTH, updateEncoderA);
+    l = new Motor(pi_22, pi_23, pi_27, pi_17, pi_4, 10.0, 100.0, 0.0);
+    r = new Motor(pi_10, pi_9, pi_25, pi_20, pi_21, 10.0, 100.0, 0.0);
+    b = new Motor(pi_6, pi_5, pi_12, pi_11, pi_26, 10.0, 100.0, 0.0);
+    
+    wiringPiISR(l->motor_encA, INT_EDGE_BOTH, updateEncoderL);
+    wiringPiISR(l->motor_encB, INT_EDGE_BOTH, updateEncoderL);
 
+    wiringPiISR(r->motor_encA, INT_EDGE_BOTH, updateEncoderR);
+    wiringPiISR(r->motor_encB, INT_EDGE_BOTH, updateEncoderR);
+
+    wiringPiISR(b->motor_encA, INT_EDGE_BOTH, updateEncoderB);
+    wiringPiISR(b->motor_encB, INT_EDGE_BOTH, updateEncoderB);
     while(true){
-        std::cout<<a->read()<<"\n";
+        std::cout<<"L: "<<l->read()<<" R: "<<r->read()<<" B: "<<b->read()<<"\n";
         delay(10);
         // softPwmWrite(pi_27, 100);
         // if((count/10000)%2){
