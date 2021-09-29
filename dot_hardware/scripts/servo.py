29fb8ce1331089f579ac782605c690b17332fe8e
hex-plex <sendhilsom7@gmail.com>
2021-09-30 00:50:58 +0530

    added Servo Server

--- dot_hardware/scripts/servo.py
+++ dot_hardware/scripts/servo.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
  
-from gpiozero import Servo
-from time import sleep
-from gpiozero.pins.pigpio import PiGPIOFactory
-factory = PiGPIOFactory()
 
-servo1 = Servo(14, initial_value=0, pin_factory=factory)
-servo2 = Servo(15, initial_value=0, pin_factory=factory)
+import rospy
+from geometry_msgs.msg import Point
+
+import math
+from gpiozero import Servo
+from gpiozero.pins.pigpio import PiGPIOFactory
+
+factory = None
+servo1 = None
+servo2 = None
+
 
 m = 0.5
 
@@ -15,21 +20,68 @@ val2=0
 
 d1 = 0.005
 d2 = 0.005
-try:
-	while True:
-		sleep(0.005)
-		val1+=d1
-		val2+=d2
-		if(val1>=m or val1<=(-m)):
-			d1*=-1
-		else:
-			servo1.value = val1
-		if(val2>=m or val2<=(-m)):
-			d2*=-1
-		else:
-			servo2.value = val2
-except KeyboardInterrupt:
-	print("Program Stopped")
 
-servo1.detach()
-servo2.detach()
+p = Point()
+p.x = 0
+p.y = 0
+p.z = 0
+
+def instruct_callback(msg):
+	p = msg
+	p.z = 0
+	mag = math.sqrt(p.x**2 + p.y**2)
+	if mag < 0.1: ## check this parameter carefully
+		new_goal = False
+	else: 
+		new_goal = True
+	p.x /= mag
+	p.y /= mag
+	
+def coor2ang(x, y):
+	return x,y
+
+def init():
+	global factory, servo1, servo2
+
+	factory = factory or PiGPIOFactory()
+	servo1 = servo1 or Servo(14, initial_value=0, pin_factory=factory)
+	servo2 = servo2 or Servo(15, initial_value=0, pin_factory=factory)
+
+
+def main_loop():
+	global factory, servo1, servo2, p, m, val1, val2, d1, d2
+
+	rate = rospy.Rate(50)
+	while not rospy.is_shutdown():
+		try:
+			if new_goal:
+				v1, v2 = coor2ang(p.x, p.y)
+				servo1.value = v1
+				servo2.value = v2
+				new_goal = False
+			else: 
+				val1+=d1
+				val2+=d2
+				if(val1>=m or val1<=(-m)):
+					d1*=-1
+				else:
+					servo1.value = val1
+				if(val2>=m or val2<=(-m)):
+					d2*=-1
+				else:
+					servo2.value = val2
+			rate.sleep()
+		except rospy.ROSInterruptException:
+			rospy.logerr("Shutting Down Servo Server")
+		except rospy.ROSTimeMovedBackwardsException:
+			rospy.logerr("Here goes time")
+	
+	servo1.detach()
+	servo2.detach()
+
+if __name__ == "__main__":
+	rospy.init_node('servo_server', anonymous=True)
+
+	init()
+	rospy.Subscriber("servo_dir", Point, instruct_callback )
+	main_loop()
