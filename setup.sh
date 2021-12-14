e37556384066c988578c0945c4688b12197b8251
hex-plex <sendhilsom7@gmail.com>
2021-12-14 14:42:56 +0530

    added all changes

--- setup.sh
+++ setup.sh
@@ -1,2 +1,13 @@
 #! /bin/sh
-echo "alias Fcatkin_make='cd src/Swarm-Bot-Hardware && git pull origin master && cd ../.. && catkin_make'" >> ~/.bashrc
+if [ "$ROS_DOT_SETUPED" ];
+then
+    echo "The package is already setup!!, if you are facing any problem feel free to rise an issue"
+else
+    echo "alias Fcatkin_make='cd src/Swarm-Bot-Hardware && git pull && cd ../.. && catkin_make'" >> ~/.bashrc
+    echo "dtoverlay=disable-bt" >> /boot/config.txt
+    echo "enable_uart=1" >> /boot/config.txt
+    echo "gpu_mem=16" >> /boot/config.txt
+    sudo systemctl enable pigpiod && sudo systemctl disable hciuart
+    echo "export ROS_DOT_SETUPED=1" >> ~/.bashrc
+    echo "Reboot the raspi now"
+fi
