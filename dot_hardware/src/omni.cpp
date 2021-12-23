ed9e0a8ab4b91d49d992e2000d2f3735d420091b
hex-plex <sendhilsom7@gmail.com>
2021-12-24 03:19:32 +0530

    optimize

--- dot_hardware/src/omni.cpp
+++ dot_hardware/src/omni.cpp
@@ -1,5 +1,33 @@
+/*
+* file: omni.cpp
+* author: Somnath Sendhil Kumar
+* data: 5th Dec 2021
+*/
+
+#include <stdio.h>
+#include <iostream>
+#include <atomic>
+#include <stdlib.h>
+#include <string>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/signal.h>
+#include <errno.h>
+#include <termios.h>
+#include <cstdlib>
+
 #include <ros/ros.h>
+#include <std_msgs/Float64.h>
+#include <sensor_msgs/JointState.h>
+#include <std_msgs/Empty.h>
+
+#define BUF_SIZE 256
+
+#include <unistd.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Quaternion.h>
@@ -28,9 +56,44 @@
 
 ros::Time timeCurrent;
 ros::Time timePrevious;
-ros::Publisher pub_;
-ros::Subscriber imu_sub, cmd_vel_sub, jnt_sub;
-ros::Publisher l_pub_, r_pub_, b_pub_;
+
+ros::Publisher jnt_state_pub_;
+ros::Publisher odom_pub_;
+ros::Subscriber cmd_vel_sub_;
+ros::Subscriber stm_reset_sub_;
+sensor_msgs::JointState jnt_st;
+
+struct ttas_lock {
+  std::atomic<bool> lock_ = {false};
+
+  void unlock() {lock_.store(false);}
+
+  void lock() {
+    for (;;) {
+      if (!lock_.exchange(true, std::memory_order_acquire)) {
+        break;
+      }
+      while (lock_.load(std::memory_order_relaxed));
+    }
+  }
+};
+
+
+void signal_handler_IO (int status);   /* definition of signal handler */
+void inp_parse(int res);
+void lf_wheel_callback(const std_msgs::Float64& msg);
+void rt_wheel_callback(const std_msgs::Float64& msg);
+void bk_wheel_callback(const std_msgs::Float64& msg);
+
+void stm_reset(const std_msgs::Empty& msg);
+
+struct ttas_lock write_lock;
+int n;
+int fd;
+int connected;
+struct termios termAttr;
+struct sigaction saio;
+char buf[BUF_SIZE];
 
 bool imu_flag=true;
 double yaw_offset = 0;
@@ -165,7 +228,7 @@ void publishOdom(){
     odom.twist.twist.angular.y= 0 ;
     odom.twist.twist.angular.z= odom_.wz ;
 
-    pub_.publish(odom);
+    odom_pub_.publish(odom);
 }
 void velocity_callback(const geometry_msgs::Twist& msg){
     vx = msg.linear.x;
@@ -192,23 +255,64 @@ void imu_callback(const sensor_msgs::Imu::ConstPtr& msg){
     odom_.theta = p_.yaw - yaw_offset;
 }
 
-// class OmniDriver{
-//     private:
-
-//     public:
-//         double l_w_dir=1, b_w_dir=1, r_w_dir=1;
-//         OmniDriver(ros::NodeHandle* n){
-
-//         }
-// };
 
 int main(int argc, char** argv){
 
     ros::init(argc, argv, "omnidrive");
     ros::NodeHandle n("");
     timePrevious = ros::Time::now();
-    int debug = getenv("DEBUG")?atoi(getenv("DEBUG")):0;
+    jnt_state_pub_ = n.advertise<sensor_msgs::JointState>("joint_state",100);
     device_name_ = getenv("ROS_DEVICE_NAME");
+    jnt_st.header.frame_id = device_name_+"/base_link";
+    jnt_st.header.stamp = ros::Time::now();
+    jnt_st.name = std::vector<std::string>(3,"none");
+    jnt_st.name[0] = "left_joint";
+    jnt_st.name[2] = "right_joint";
+    jnt_st.name[1] = "back_joint";
+
+    jnt_st.position = std::vector<double> (3,0);
+    jnt_st.velocity = std::vector<double> (3,0);
+    jnt_st.effort = std::vector<double> (3,0);
+
+    fd = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY | O_NDELAY);
+    if (fd == -1)
+    {
+       perror("open_port: Unable to open /dev/ttyAMA0\n");
+       exit(1);
+    }
+
+    saio.sa_handler = signal_handler_IO;
+    saio.sa_flags = 0;
+    saio.sa_restorer = NULL; 
+    sigaction(SIGIO,&saio,NULL);
+
+    fcntl(fd, F_SETFL, FNDELAY);
+    fcntl(fd, F_SETOWN, getpid());
+    fcntl(fd, F_SETFL,  O_ASYNC ); /**<<<<<<------This line made it work.**/
+
+    tcgetattr(fd,&termAttr);
+    //baudRate = B115200;          /* Not needed */
+    cfsetispeed(&termAttr,B2000000);
+    cfsetospeed(&termAttr,B2000000);
+    termAttr.c_cflag &= ~PARENB;
+    termAttr.c_cflag &= ~CSTOPB;
+    termAttr.c_cflag &= ~CSIZE;
+    termAttr.c_cflag |= CS8;
+    termAttr.c_cflag |= (CLOCAL | CREAD);
+    termAttr.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+    termAttr.c_iflag &= ~(IXON | IXOFF | IXANY);
+    termAttr.c_oflag &= ~OPOST;
+    tcsetattr(fd,TCSANOW,&termAttr);
+    // printf("UART1 configured....\n");
+    usleep(10000);
+    connected = 1;
+
+    write_lock.lock();
+    stm_reset_sub_ = n.subscribe("stm_comm/reset", 1, stm_reset);
+    write_lock.unlock();
+
+
+    int debug = getenv("DEBUG")?atoi(getenv("DEBUG")):0;
     // double wheel_speed = getenv("WSP")?atoi(getenv("WSP")):10;
     double hz=100;
     ros::Rate rate(hz);
@@ -216,13 +320,9 @@ int main(int argc, char** argv){
     // OmniDriver* div;
     // div = new OmniDriver(&n);
 
-    cmd_vel_sub = n.subscribe("cmd_vel", 1000, velocity_callback);
-    jnt_sub = n.subscribe("joint_state", 1000, jnt_state_callback);
+    cmd_vel_sub_ = n.subscribe("cmd_vel", 1000, velocity_callback);
     //imu_sub = n.subscribe("imu/data", 100, imu_callback); // use the one with madwigk filter not this
-    pub_ = n.advertise<nav_msgs::Odometry>("odom", 50) ;
-    l_pub_ = n.advertise<std_msgs::Float64>("velocity_controller/left_joint_vel_controller/command", 10);
-    r_pub_ = n.advertise<std_msgs::Float64>("velocity_controller/right_joint_vel_controller/command", 10);
-    b_pub_ = n.advertise<std_msgs::Float64>("velocity_controller/back_joint_vel_controller/command", 10);
+    odom_pub_ = n.advertise<nav_msgs::Odometry>("odom", 50) ;
     long int i = 0;
 
     while(ros::ok()){
@@ -243,11 +343,11 @@ int main(int argc, char** argv){
         // wheel_.rpos = wheel_.rpos + div->r_w_dir*wheel_speed*v3*dt_/R;
         std_msgs::Float64 wheel_vel;
         wheel_vel.data = v1;
-        l_pub_.publish(wheel_vel);
+        lf_wheel_callback(wheel_vel);
         wheel_vel.data = v2;
-        b_pub_.publish(wheel_vel);
+        bk_wheel_callback(wheel_vel);
         wheel_vel.data = v3;
-        r_pub_.publish(wheel_vel);
+        rt_wheel_callback(wheel_vel);
 
         publishOdom();
 
@@ -255,5 +355,124 @@ int main(int argc, char** argv){
         rate.sleep();
         
     }
+    printf("exiting stm-connect");
+    close(fd);
+    exit(0);
     return 0;
+}
+
+void signal_handler_IO (int status)
+{
+    // printf("received data from UART.\n");
+    uint8_t x;
+    int result;
+    
+    result = read(fd, buf, (int)BUF_SIZE);
+    buf[result]= 0;
+    inp_parse(result);
+    //printf("%s\n",buf);
+    
+}
+
+void inp_parse(int res){
+
+    int result=1;
+    /// add your data here to the msg
+    buf[res];
+    std::string inter=""; 
+    int i=0;
+    while(buf[i]=='|')i++;
+    //printf("%s",buf);
+    for(;i<res;i++){
+        //printf("%c",buf[i]);
+	    if(buf[i]==0)break;
+        if(buf[i]=='|'){
+            //std::cout<<inter<<"\n";
+            if(inter!=""){
+                int n = inter.length();
+                if(n<=3) break;
+                int st = inter.find(":");
+                if (st==-1) break;
+                try{
+                    switch(inter[st-3]){
+                        case 'l':
+                            if(inter[st-1]=='v')
+                                jnt_st.velocity[0] = std::stod(inter.substr(st+1));
+                            else if(inter[st-1]=='p')
+                                jnt_st.position[0] = std::stod(inter.substr(st+1));
+                            break;
+                        case 'r':
+                            if(inter[st-1]=='v')
+                                jnt_st.velocity[2] = std::stod(inter.substr(st+1));
+                            else if(inter[st-1]=='p')
+                                jnt_st.position[2] = std::stod(inter.substr(st+1));
+                            break;
+                        case 'b':
+                            if(inter[st-1]=='v')
+                                jnt_st.velocity[1] = std::stod(inter.substr(st+1));
+                            else if(inter[st-1]=='p')
+                                jnt_st.position[1] = std::stod(inter.substr(st+1));
+                            break;
+                    }
+                } catch(std::invalid_argument& e){
+                    ROS_WARN("Failed to parse STM input %s", inter);
+                }
+            } else {
+                break;
+            }
+            inter = "";
+        } else {
+            inter.push_back(buf[i]);
+        }
+    }
+    //std::cout<<inter<<"\n";
+    if(result==-1) // Validate the string
+        return;
+
+    jnt_state_callback(jnt_st);
+}
+
+void lf_wheel_callback(const std_msgs::Float64& msg){
+    double value = msg.data;
+    //std::cout<<value<<"\n";
+    char msg_data[16];
+    sprintf(msg_data, "l_v:%.3f|\r\n",value);
+    int n = strlen(msg_data);
+    //printf("%s, %d",msg_data,n);
+    write_lock.lock();
+    write(fd, msg_data, n);
+    write_lock.unlock();
+    //usleep(8*100);
+}
+
+void rt_wheel_callback(const std_msgs::Float64& msg){
+    double value = msg.data;
+    char msg_data[16];
+    sprintf(msg_data, "r_v:%.3f|\r\n",value);
+    int n = strlen(msg_data);
+    //printf("%s, %d",msg_data,n);
+    write_lock.lock();
+    write(fd, msg_data, n);
+    write_lock.unlock();
+    //usleep(8*100);
+}
+
+void bk_wheel_callback(const std_msgs::Float64& msg){
+    double value = msg.data;
+    char msg_data[16];
+    sprintf(msg_data, "b_v:%.3f|\r\n",value);
+    int n = strlen(msg_data);
+    //printf("%s, %d",msg_data,n);
+    write_lock.lock();
+    write(fd, msg_data, n);
+    write_lock.unlock();
+    //usleep(8*100);
+}
+
+void stm_reset(const std_msgs::Empty& msg){
+    write_lock.lock();
+    write(fd, "|\n", 2);
+    usleep(8*25);
+    tcflush(fd, TCIOFLUSH);
+    write_lock.unlock();
 }
